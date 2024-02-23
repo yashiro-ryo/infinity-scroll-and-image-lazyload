2a2243e6ce75f398a83a24689aaf503facb66a0b
yashiroryo <>
2024-02-23 17:08:19 +0900

    chore: CSSの調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>Scroll Lazyload</h1>
+      </div>
+      <div>
+        <div className="list-container">
+          <ul className="list-group">
+            <li className="list-item">item 1</li>
+            <li className="list-item">item 2</li>
+            <li className="list-item">item 3</li>
+            <li className="list-item">item 4</li>
+            <li className="list-item">item 5</li>
+            <li className="list-item">item 6</li>
+            <li className="list-item">item 7</li>
+            <li className="list-item">item 8</li>
+            <li className="list-item">item 9</li>
+            <li className="list-item">item 10</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
