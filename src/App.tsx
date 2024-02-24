f7a3cf87d9149eea65d0cdf2f76ea81f866f1774
yashiroryo <>
2024-02-24 12:16:56 +0900

    feat: 無限スクロールを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [listItem, setListItem] = useState<Array<string>>([]);
+  const [lastIndex, setLastIndex] = useState(1);
+  const [isLoading, setLoading] = useState(false);
+  const [firstItemLoaded, setFirstItemLoaded] = useState(false);
+  const PER_LIST_ITEM = 20;
+  const READ_MORE_MARGIN = 60;
+
+  useEffect(() => {
+    if (!firstItemLoaded) {
+      getListItem();
+    }
+  }, []);
+
+  const getListItem = () => {
+    if (isLoading) {
+      return;
+    }
+    setLoading(true);
+    // API request を想定
+    setTimeout(() => {
+      const addedListItem = listItem.concat(createItem());
+      setListItem(addedListItem);
+      setLastIndex(lastIndex + PER_LIST_ITEM);
+      setLoading(false);
+      setFirstItemLoaded(true);
+    }, 500);
+  };
+
+  const createItem = (): Array<string> => {
+    // ダミーリストアイテムを生成
+    const array = [];
+    for (let i = lastIndex; i < lastIndex + PER_LIST_ITEM; i++) {
+      array.push(`item-${i}`);
+    }
+    return array;
+  };
+
+  const onScroll = () => {
+    loadMoreIfNeed();
+  };
+
+  const loadMoreIfNeed = () => {
+    // リスト最後の要素を取得
+    const bottomListItem = document.getElementById(
+      `list-item-${lastIndex - 1}`
+    );
+    if (bottomListItem === null) {
+      return;
+    }
+    // タイトル部分 + スクロール表示部の高さ + リスト1つ分
+    if (
+      500 + 100 + READ_MORE_MARGIN >
+      bottomListItem.getBoundingClientRect().top
+    ) {
+      getListItem();
+    }
+  };
+
   return (
     <div>
-      <div>
+      <div className="title">
         <h1>Scroll Lazyload</h1>
       </div>
       <div>
-        <div className="list-container">
+        <div className="list-container" onScroll={onScroll}>
           <ul className="list-group">
-            <li className="list-item">item 1</li>
-            <li className="list-item">item 2</li>
-            <li className="list-item">item 3</li>
-            <li className="list-item">item 4</li>
-            <li className="list-item">item 5</li>
-            <li className="list-item">item 6</li>
-            <li className="list-item">item 7</li>
-            <li className="list-item">item 8</li>
-            <li className="list-item">item 9</li>
-            <li className="list-item">item 10</li>
+            {listItem.map((item, index) => {
+              return (
+                <li
+                  key={`list-item-${index}`}
+                  className="list-item"
+                  id={`list-${item}`}
+                >
+                  {item}
+                </li>
+              );
+            })}
           </ul>
         </div>
+        <button onClick={getListItem}>Add List Item</button>
       </div>
     </div>
   );
