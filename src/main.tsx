e0c30113dfcc7cd94332cc06b622dae828a39233
yashiroryo <>
2024-03-01 22:40:58 +0900

    feat: 画像一覧を表示できるところまで実装

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { InfinityScroll } from "./pages/InfinityScroll/index"
 import { Lazyload } from "./pages/Lazyload/index"
-import { Index } from "./pages/Index"
+import { Index } from "./pages/index"
 
 const router = createBrowserRouter([
   {
--- src/pages/Lazyload/index.tsx
+++ src/pages/Lazyload/index.tsx
@@ -1,5 +1,36 @@
-import './style.css'
+import "./style.css"
 
 export const Lazyload = () => {
-  return <div>Lazylaod</div>
+  const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const cols = [1, 2, 3, 4]
+  return (
+    <div>
+      <div>
+        <h1>LazyLoad</h1>
+      </div>
+      <div className="body">
+        <div className="container">
+          {rows.map((_, rowIndex) => {
+            return (
+              <div className="container-row" key={`${rowIndex}`}>
+                {cols.map((_, colIndex) => {
+                  return (
+                    <div
+                      className="img-wrapper"
+                      key={`${rowIndex}-${colIndex}`}
+                    >
+                      <img
+                        src={`/${(rowIndex + 1) * (colIndex + 1)}.png`}
+                        loading="lazy"
+                      />
+                    </div>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
 }
