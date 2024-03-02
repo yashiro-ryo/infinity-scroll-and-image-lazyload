c7ab4b5576b7c2b7c9522cd0495e41097b9b0f68
yashiroryo <>
2024-03-02 15:20:04 +0900

    feat: 画像がスクロールして描画内に入った時にロードが開始するように実装

--- src/pages/Lazyload/index.tsx
+++ src/pages/Lazyload/index.tsx
@@ -1,8 +1,50 @@
+import { useEffect } from "react"
 import "./style.css"
 
 export const Lazyload = () => {
   const rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   const cols = [1, 2, 3, 4]
+  let lastCalledAt = Date.now()
+  const INTERVAL = 100
+
+  const loadImgIfNeed = () => {
+    const scrollBottom = window.innerHeight + window.scrollY
+    const imgWrappers = document.getElementsByClassName("img-wrapper")
+    for (let i = 0; i < imgWrappers.length; i++) {
+      const imgWrapper = imgWrappers.item(i) as HTMLDivElement
+      const imgWrapperBottom = imgWrapper.offsetTop
+      if (imgWrapperBottom < scrollBottom) {
+        const imgElem = imgWrapper.childNodes[0] as HTMLImageElement
+        if (imgElem === null) {
+          continue
+        }
+        const imgSrc = imgElem.getAttribute("data-img-no")
+        imgElem.src = `/${imgSrc}.png`
+      }
+    }
+  }
+
+  const throttle = () => {
+    const nowTime = Date.now()
+    if (lastCalledAt + INTERVAL < nowTime) {
+      onScroll()
+      lastCalledAt = Date.now()
+    }
+  }
+
+  const onScroll = () => {
+    console.log("scroll")
+    loadImgIfNeed()
+  }
+
+  useEffect(() => {
+    loadImgIfNeed()
+    window.addEventListener("scroll", throttle)
+    return () => {
+      window.removeEventListener("scroll", throttle)
+    }
+  }, [])
+
   return (
     <div>
       <div>
@@ -20,8 +62,10 @@ export const Lazyload = () => {
                       key={`${rowIndex}-${colIndex}`}
                     >
                       <img
-                        src={`/${(rowIndex + 1) * (colIndex + 1)}.png`}
-                        loading="lazy"
+                        className="img"
+                        data-img-no={`${
+                          rowIndex * 3 + (rowIndex + 1) + colIndex
+                        }`}
                       />
                     </div>
                   )
